5bc032eafe81ce271b78cfc5996664629444413c
Antonio Pierro <antonio.pierro@gmail.com>
2020-07-16 16:22:25 +0200

    adding terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: fs.readdirSync("./tests").map((s) => `./tests/${s}`),
@@ -24,6 +25,10 @@ module.exports = {
             filename: "index.html",
         }),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
     devServer: {
         hot: true,
         port: 8080,
