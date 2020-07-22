62aa520a83e528e7fc0f54e831a66857783cf2d8
Antonio Pierro <antonio.pierro@gmail.com>
2020-07-23 01:34:48 +0200

    addin getTotalGoals method

--- src/rest.js
+++ src/rest.js
@@ -1,16 +1,41 @@
 const https = require("https");
+const axios = require("axios");
 
 module.exports = {
     //https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
     getTotalGoals: function (team, year) {
-        const url = `https://jsonmock.hackerrank.com/api/football_matches?year=${year}&team1=${team}&page=1`;
+        const prefix = "https://jsonmock.hackerrank.com/api/football_matches";
+        urls = [
+            `${prefix}?year=${year}&team1=${team}&page=1`,
+            `${prefix}?year=${year}&team2=${team}&page=1`,
+        ];
+        const promises = urls.map((url) => axios.get(url));
+        const getGoals = (arr, key) => {
+            key = key & 1 ? "team2goals" : "team1goals";
+            return arr.reduce((a, c) => a + Number(c[key]), 0);
+        };
 
-        let result = 0;
-
-        return fetch(url)
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+        return axios.all(promises).then((results) => {
+            let temp = results.map((r, i) => getGoals(r.data.data, i));
+            return temp.reduce((a, c) => a + c, 0);
+        });
     },
 
     getWinnerTotalGoals: function (competition, year) {},
+
+    delay: function (time) {
+        return new Promise((resolve, reject) => {
+            if (isNaN(time)) {
+                reject(new Error("Not a valid number"));
+            } else {
+                setTimeout(resolve(time), time);
+            }
+        });
+    },
+
+    async: function (value) {
+        return new Promise(function (resolve) {
+            setTimeout(() => resolve(value), 0);
+        });
+    },
 };
--- tests/rest.test.js
+++ tests/rest.test.js
@@ -3,13 +3,26 @@ const ans = require("../src/rest");
 
 describe("Rest", function () {
     it("It must return an integer denoting the total number of goals scored in all matches in the given competition by the team who won the competition", () => {
-        // let res = await ans.getTotalGoals("UEFA Champions League", "2011");
-        // expect(res).to.eql(28);
-        // ans.getTotalGoals("UEFA Champions League", "2011").then(function (
-        //     result
-        // ) {
-        //     expect(result).to.have.length(5);
-        //     done();
+        ans.getTotalGoals("Barcelona", 2011).then(function (result) {
+            flag = result;
+            expect(flag).to.eql(35);
+        });
+
+        // ans.getTotalGoals().then(function (result) {
+        //     expect(flag).to.eql(35);
         // });
+    });
+    it("it should return a number", () => {
+        // expect(ans.delay(2000).then(() => console.log(0))).to.eql(0);
+
+        ans.async("success").then(function (result) {
+            flag = result;
+            expect(flag).to.eql("success");
+        });
+
+        ans.delay(1000).then(function (result) {
+            flag = result;
+            expect(flag).to.eql(1000);
+        });
     });
 });
