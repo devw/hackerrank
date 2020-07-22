60d4c0fc963a600f59d488122548341e0d37f28d
Antonio Pierro <antonio.pierro@gmail.com>
2020-07-22 18:43:51 +0200

    fixing loop in minimumSwaps

--- src/arrays.js
+++ src/arrays.js
@@ -2,18 +2,18 @@
 module.exports = {
     //https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
     minimumBribes: function (q) {
-        let res = 0;
-        for (let i = q.length; i >= 0; i--) {
+        let count = 0;
+        for (let i = q.length - 1; i >= 0; i--) {
             if (q[i] - (i + 1) > 2) return "Too chaotic";
-            for (let j = q[i] - 2; j < i; j++) if (q[j] > q[i]) res++;
+            for (let j = q[i] - 2; j < i; j++) if (q[j] > q[i]) count++;
         }
-        return res;
+        return count;
     },
 
     minimumSwaps: function (arr) {
         let count = 0;
         for (let i = 0; i < arr.length; i++) {
-            while (arr[i] != i + 1) {
+            while (arr[i] !== i + 1) {
                 let j = arr[i] - 1;
                 [arr[i], arr[j]] = [arr[j], arr[i]];
                 count++;
