3731d5ba4f470f58469a460fefedb5404b124af1
Antonio Pierro <antonio.pierro@gmail.com>
2020-07-29 16:16:06 +0200

    updating medium-algorithms tests

--- tests/medium-algorithms.test.js
+++ tests/medium-algorithms.test.js
@@ -2,22 +2,26 @@ const expect = require("chai").expect;
 const answers = require("../src/medium-algorithms");
 
 describe("Medium Problem Solving Algorithms", function () {
-    it("should return an integer array where each element represents Alice's rank.", function () {
-        expect(
-            answers.climbingLeaderboard([10, 5, 4], [3, 5, 6, 6, 8])
-        ).to.eql([4, 2, 2, 2, 2]);
-        expect(
-            answers.climbingLeaderboard(
-                [100, 100, 50, 40, 40, 20, 10],
-                [5, 25, 50, 120]
-            )
-        ).to.eql([6, 4, 2, 1]);
-        expect(
-            answers.climbingLeaderboard(
-                [100, 90, 90, 80, 75, 60],
-                [50, 65, 77, 90, 102]
-            )
-        ).to.eql([6, 5, 4, 2, 1]);
+    it("It should return an integer array where each element represents Alice's rank.", function () {
+        let scores = [10, 5, 4];
+        let alice = [3, 5, 6, 6, 8];
+        let result = [4, 2, 2, 2, 2];
+        expect(answers.climbingLeaderboard(scores, alice)).to.eql(result);
+
+        scores = [100, 100, 50, 40, 40, 20, 10];
+        alice = [5, 25, 50, 120];
+        result = [6, 4, 2, 1];
+        expect(answers.climbingLeaderboard(scores, alice)).to.eql(result);
+
+        scores = [100, 90, 90, 80];
+        alice = [70, 80, 105];
+        result = [4, 3, 1];
+        expect(answers.climbingLeaderboard(scores, alice)).to.eql(result);
+
+        scores = [100, 90, 90, 80, 75, 60];
+        alice = [50, 65, 77, 90, 102];
+        result = [6, 5, 4, 2, 1];
+        expect(answers.climbingLeaderboard(scores, alice)).to.eql(result);
     });
 
     it("should return a string, either Yes or No.", function () {
