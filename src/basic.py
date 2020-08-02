e14b2f630cdf4b77f41781258a7d258edaf66986
Antonio Pierro <antonio.pierro@gmail.com>
2020-08-02 06:52:59 +0200

    adding multiset problem

--- src/basic.py
+++ src/basic.py
@@ -4,8 +4,7 @@ def reverse_words_order_and_swap_cases(sentence):
     return " ".join(sentence.split(" ")[::-1]).swapcase()
 
 class Car:
-    # The constructor for Car must take two arguments. The first of them is its maximum speed, and the second one is a string that denotes the units in which the speed is given: either "km/h" or "mph".
-    # The class must be implemented to return a string based on the arguments. For example, if car is an object of class Car with a maximum speed of 120, and the unit is "km/h", then printing car prints the following string: "Car with the maximum speed of 120 km/h", without quotes. If the maximum speed is 94 and the unit is "mph", then printing car prints in the following string: "Car with the maximum speed of 94 mph", without quotes.
+    # The constructor for Car must take two arguments. The first of them is its maximum speed, and the second one is a string that denotes the units in which the speed is given: either "km/h" or "mph". The class must be implemented to return a string based on the arguments. For example, if car is an object of class Car with a maximum speed of 120, and the unit is "km/h", then printing car prints the following string: "Car with the maximum speed of 120 km/h", without quotes. If the maximum speed is 94 and the unit is "mph", then printing car prints in the following string: "Car with the maximum speed of 94 mph", without quotes.
     def __init__(self, speed, unitSpeed):
         self.speed = speed
         self.unitSpeed = unitSpeed
@@ -13,9 +12,33 @@ class Car:
         return f"Car with the maximum speed of {speed} {unitSpeed}"
 
 class Boat:
-    # The constructor for Boat must take a single argument denoting its maximum speed in knots.
-    # The class must be implemented to return a string based on the argument. For example, if boat is an object of class Boat with a maximum speed of 82, then printing boat prints the following string: "Boat with the maximum speed of 82 knots", without quotes.
+    # The constructor for Boat must take a single argument denoting its maximum speed in knots. The class must be implemented to return a string based on the argument. For example, if boat is an object of class Boat with a maximum speed of 82, then printing boat prints the following string: "Boat with the maximum speed of 82 knots", without quotes.
     def __init__(self, speed):
         self.speed = speed
     def __new__(cls, speed):
         return f"Boat with the maximum speed of {speed} knots"
+
+
+class Multiset():
+    # A multiset is the same as a set except that an element might occur more than once in a multiset. Implement a multiset data structure in Python. 
+    def __init__(self):
+        self.multiset = []
+
+    def add(self, val):
+        # adds one occurrence of val from the multiset, if any
+        self.multiset.append(val)
+
+    def remove(self, val):
+        # removes one occurrence of val from the multiset, if any
+        self.multiset.remove(val)
+
+    def __contains__(self, val):
+        # returns True when val is in the multiset, else returns False
+        return val in self.multiset
+
+    def __len__(self):
+        # returns the number of elements in the multiset
+        return len(self.multiset)
+
+    def __repr__(self):
+        return f"{self.multiset}"
--- src/test_basic.py
+++ src/test_basic.py
@@ -22,11 +22,31 @@ class TestReverseWords(unittest.TestCase):
         self.assertEqual(basic.reverse_words_order_and_swap_cases("rUns dOg"), "DoG RuNS")
         self.assertEqual(basic.reverse_words_order_and_swap_cases("aWESOME is cODING"), "Coding IS Awesome")
     
-    def test_car_object(self):
+    def test_car_class(self):
         self.assertEqual(basic.Car(251, "km/h"), "Car with the maximum speed of 251 km/h")
     
-    def test_boat_object(self):
+    def test_boat_class(self):
         self.assertEqual(basic.Boat(101), "Boat with the maximum speed of 101 knots")
+
+    def test_multiset_class(self):
+        def performOperations(operations):
+            m = basic.Multiset()
+            result = []
+            for op_str in operations:
+                elems = op_str.split()
+                if elems[0] == 'size':
+                    result.append(len(m))
+                else:
+                    op, val = elems[0], int(elems[1])
+                    if op == 'query':
+                        result.append(val in m)
+                    elif op == 'add':
+                        m.add(val)
+                    elif op == 'remove':
+                        m.remove(val)
+            return result
+        res = performOperations(['query 1', 'add 1', 'query 1', 'remove 1','query 1', 'add 2', 'add 2', 'size', 'query 2', 'remove 2', 'query 2', 'size'])
+        self.assertEqual(res, [False, True, False, 2, True, True, 1]) 
 
 if __name__ == '__main__':
     unittest.main()
