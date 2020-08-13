1624cc4ac5ce10c91011d55b6d2afe4b46730352
Antonio Pierro <antonio.pierro@gmail.com>
2020-08-13 11:15:59 +0200

    updating maxSubsetSum algo

--- src/dynamic-programming.js
+++ src/dynamic-programming.js
@@ -2,13 +2,14 @@ module.exports = {
     // https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
     // medium
     maxSubsetSum: function (arr) {
-        let inclusive, exclusive, past_inclusive;
-        for (let i = 0; i < arr.length; i++) {
-            past_inclusive = inclusive || 0;
-            exclusive = exclusive || 0;
-            inclusive = Math.max(exclusive + arr[i], past_inclusive);
-            exclusive = past_inclusive;
+        const dp = [];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (let i = 2; i < arr.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i], dp[i - 2], arr[i]);
         }
-        return Math.max(inclusive, exclusive);
+
+        return dp.slice(-1)[0]; // last element of array
     },
 };
--- tests/dynamic-programming.test.js
+++ tests/dynamic-programming.test.js
@@ -3,6 +3,11 @@ const answers = require("../src/dynamic-programming");
 
 describe("Medium Problem Solving Algorithms", function () {
     it("It should return an integer representing the maximum subset sum for the given array.", function () {
+        expect(answers.maxSubsetSum([5, 3, -7, 8, 10])).to.eql(15);
         expect(answers.maxSubsetSum([3, 5, -7, 8, 10])).to.eql(15);
+        expect(answers.maxSubsetSum([-2, 1, 3, -4, 5])).to.eql(8);
+        expect(answers.maxSubsetSum([8, -3, 3, -4, 5])).to.eql(16);
+        expect(answers.maxSubsetSum([8006, -3505, 3, -4, 5])).to.eql(8014);
+        expect(answers.maxSubsetSum([8006, -3505, -3, -4, -5])).to.eql(8006);
     });
 });
