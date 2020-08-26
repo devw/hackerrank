ad8f3ae46b95a954c456137e04cb7c555964715b
Antonio Pierro <antonio.pierro@gmail.com>
2020-08-26 10:10:35 +0200

    refactoring proxies

--- src/proxies.js
+++ src/proxies.js
@@ -1,8 +1,6 @@
 module.exports = {
     // Proxies in JavaScript (ES6).mp4
     handler: {
-        get(target, key) {
-            return key in target ? target[key] : 37;
-        },
+        get: (target, key) => (key in target ? target[key] : 0),
     },
 };
--- tests/proxies.test.js
+++ tests/proxies.test.js
@@ -1,7 +1,7 @@
 const expect = require("chai").expect;
 const ans = require("../src/proxies");
 
-describe("Proxies", () => {
+describe("Proxy is about Reflection through intercession - wrapping objects and intercepting their behaviours through traps. Intercession allows to redefine the semantics of some language operations.", () => {
     it("It should not return undefined.", () => {
         let target = {};
         let proxy = new Proxy(target, ans.handler);
